0c6fb363fe088d79566b14d62c57d081596ab1bb
bengquist <engquist_blake@yahoo.com>
2019-06-30 14:33:09 -0500

    update theme colors

--- components/styles/theme.js
+++ components/styles/theme.js
@@ -1,12 +1,14 @@
 export const theme = {
   colors: {
-    primary: "#DAF2FC",
-    secondary: "#009CDE",
-    accent: "#104C97",
-    gray: "#E8E8E8",
-    white: "#fff"
+    primary: "rgb(184,38,1)",
+    secondary: "rgb(131,23,114)",
+    accent: "rgb(6,47,79)",
+    black: "rgb(50,50,50)",
+    gray: "rgb(232,232,232)",
+    white: "rgb(255,255,255)"
   },
   shadows: {
-    soft: "0px 3px 6px rgba(0, 0, 0, 0.15)"
+    soft: "0px 3px 6px rgba(0, 0, 0, 0.15)",
+    softColor: color => `0px 3px 6px rgba(0, 0, 0, ${color})`
   }
 };
