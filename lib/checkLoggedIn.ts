52cffc6438e8cf09864ebab05caac4dfcd7245e5
bengquist <engquist_blake@yahoo.com>
2019-08-01 23:13:14 -0500

    get token from backend and send to backend yay

---
+++ lib/checkLoggedIn.ts
@@ -0,0 +1,19 @@
+import gql from "graphql-tag";
+
+export default async (apolloClient: any) => {
+  try {
+    const { data } = await apolloClient.query({
+      query: gql`
+        query getUser {
+          me {
+            email
+          }
+        }
+      `
+    });
+
+    return data;
+  } catch (e) {
+    return { yo: "ay" };
+  }
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,19 @@
 import React from "react";
 import Page from "../components/app/Page";
+import checkLoggedIn from "../lib/checkLoggedIn";
+import redirect from "../lib/redirect";
 
 const Home = () => <Page>Hi</Page>;
+
+Home.getInitialProps = async function(context) {
+  const { me } = await checkLoggedIn(context.apolloClient);
+
+  if (!me) {
+    // If not signed in, send them somewhere more useful
+    redirect(context, "/login");
+  }
+
+  return me;
+};
 
 export default Home;
